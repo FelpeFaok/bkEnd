49f9c09597d1ba4a734bfc7cb899f20a0b33c815
Felipe Orellana <felpe.faok@gmail.com>
2022-11-20 13:44:23 -0300

    primera entrega

--- desafio1.js
+++ desafio1.js
@@ -25,10 +25,8 @@ class ProductManager {
 
     //f() agregar productos
     addProduct = (title, descrip, price, urlImg, code, stock) => {
-
         if (this.products.some(p => p.code == code))  return 
             
-
         const product = {
             id: this.getNextProductID(),
             title,
@@ -38,7 +36,6 @@ class ProductManager {
             code,
             stock
         }
-
         this.products.push(product)
     }
 
@@ -46,18 +43,22 @@ class ProductManager {
 
 
 
-
+// crear elementos dentro del array
 const product = new ProductManager()
 
+//muestra array vacio
 console.log("Array vacio", product.getProduct());
 
+//elementos push al array
 product.addProduct("Elemento 1", "El 1 Elemento", ("$ ",1000), "img", "codigo 1", 15);
 product.addProduct("Elemento 3", "El 1 Elemento", ("$ ",1000), "img", "codigo 1", 15);
 product.addProduct("Elemento 2", "El 2 Elemento", ("$ ",1000), "img", "codigo 2", 15);
 product.addProduct("Elemento 3", "El 3 Elemento", ("$ ",1000), "img", "codigo 3", 15);
 product.addProduct("Elemento 4", "El 4 Elemento", ("$ ",1000), "img", "codigo 4", 15);
 
+//muestra los elementos dentro del array
 console.log("Los Elementos del array son: ", product.getProduct());
 
+//comprobar si existe ID
 product.getProductByID(4)
 product.getProductByID(5)
